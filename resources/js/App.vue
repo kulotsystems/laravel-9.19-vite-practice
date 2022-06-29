9b4f6ff9586fd7c7728688ae85145a2a1a73c4d6
Arvic S. Babol <kulotsystems@gmail.com>
2022-06-29 11:24:52 +0800

    Initial commit.

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Laravel 9.19 Vite Practice</h1>
+        <p>Modify <b><code>/resources/js/App.vue</code></b> while running <b><code>npm run dev</code></b></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        components: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import App    from './App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
