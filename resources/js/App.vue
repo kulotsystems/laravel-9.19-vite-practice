c11f5f039b1570ee587222f4d6079a2035048344
Arvic S. Babol <kulotsystems@gmail.com>
2022-06-29 11:33:29 +0800

    Add counter demo.

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,6 +2,11 @@
     <div>
         <h1>Laravel 9.19 Vite Practice</h1>
         <p>Modify <b><code>/resources/js/App.vue</code></b> while running <b><code>npm run dev</code></b></p>
+
+        <button @click="count--">-</button>
+        <b style="padding: 5px;">{{ count }}</b>
+        <button @click="count++">+</button>
+
     </div>
 </template>
 
@@ -10,7 +15,9 @@
         name: 'App',
         components: {},
         data() {
-            return {}
+            return {
+                count: 0
+            }
         },
         computed: {},
         methods: {}
